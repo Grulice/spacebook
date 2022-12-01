f63bcf6f4e168e0cb382d16bd59f5cffcfd3cb0e
Javid Bunyadzade <j.bunyadzade@gmail.com>
2022-12-01 15:47:25 +0400

    add a (very) minimal footer

--- components/Layout/LayoutFooter.tsx
+++ components/Layout/LayoutFooter.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const LayoutFooter: React.FC = () => {
   return (
-    <footer className='flex justify-end'>
-      <p>Copyright: Schmeta</p>
+    <footer className='flex pt-4 pb-2 px-10 justify-end'>
+      <p className='text-gray-400 text-xs'>Copyright: SchMeta, 2022</p>
     </footer>
   );
 };
